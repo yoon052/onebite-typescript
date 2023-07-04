a6700624aebc3470e527b8e00a8bd602a0b32308
yoon052 <105267838+yoon052@users.noreply.github.com>
2023-07-04 10:27:24 +0900

    feat : Function Type Expression & Call Signature

--- section4/src/chapter1.ts
+++ section4/src/chapter1.ts
@@ -1,6 +1,24 @@
 /**
  * 함수 타입 표현식
  */
-type Add = (a: number, b: number) => number;
+type Operation = (a: number, b: number) => number;
 
-const add: Add = (a, b) => a + b;
+const add: Operation = (a, b) => a + b;
+const sub: Operation = (a, b) => a - b;
+const multiply: Operation = (a, b) => a * b;
+const divide: Operation = (a, b) => a / b;
+
+/**
+ * 호출 시그니쳐
+ * (콜 시그니쳐)
+ */
+type Operation2 = {
+  (a: number, b: number): number;
+};
+
+const add2: Operation2 = (a, b) => a + b;
+const sub2: Operation2 = (a, b) => a - b;
+const multiply2: Operation2 = (a, b) => a * b;
+const divide2: Operation2 = (a, b) => a / b;
+
+//함수도 객체다
