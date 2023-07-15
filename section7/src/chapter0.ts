81c6167a95a885b3735c18e9fcbc9b5b23c2b698
yoon052 <105267838+yoon052@users.noreply.github.com>
2023-07-16 01:15:49 +0900

    feat : generic function
    New concept of TypeScript is generic that a function that can be written universally by taking several types of values as arguments along with the TPYE variable is called a generic function.

---
+++ section7/src/chapter0.ts
@@ -0,0 +1,20 @@
+/**
+ * 제네릭
+ */
+
+function func<T>(value: T): T {
+  return value;
+}
+
+let num = func(10);
+//num.toUpperCase();
+
+if (typeof num === 'number') {
+  num.toFixed();
+}
+
+let bool = func(true);
+
+let str = func('string');
+
+let arr = func<[number, number, number]>([1, 2, 3]);
